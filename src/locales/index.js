56e7e676009d3e37074520fbdd439bffa4b984ac
roopnarayankushwaha0-a11y <roopnarayankushwaha0@gmail.com>
2026-02-14 08:38:06 +0530

    Create index.js

---
+++ src/locales/index.js
@@ -0,0 +1,22 @@
+import en from './en';
+import hi from './hi';
+import es from './es';
+import fr from './fr';
+import it from './it';
+import ko from './ko';
+
+/**
+ * 🌍 Localization Registry
+ * Exports all language dictionaries referenced by LanguageContext.
+ */
+
+export const translations = {
+  en,
+  hi,
+  es,
+  fr,
+  it,
+  ko
+};
+
+export default translations;
