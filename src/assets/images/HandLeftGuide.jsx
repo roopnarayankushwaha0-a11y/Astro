cb90ce07379f0a5db33f8d9abaade7131d5f6e32
roopnarayankushwaha0-a11y <roopnarayankushwaha0@gmail.com>
2026-02-15 11:42:37 +0530

    Create HandLeftGuide.jsx

---
+++ src/assets/images/HandLeftGuide.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PalmScanGuide from './PalmScanGuide';
+
+const HandLeftGuide = (props) => (
+  <PalmScanGuide {...props} flip={false} />
+);
+
+export default HandLeftGuide;
