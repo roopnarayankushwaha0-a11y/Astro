e1cc9e6f9c3e10f4fb1a9f39eefe932f5a666c75
roopnarayankushwaha0-a11y <roopnarayankushwaha0@gmail.com>
2026-02-15 11:26:13 +0530

    Create SparkleIcon.jsx

---
+++ src/assets/icons/SparkleIcon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SparkleIcon = ({ className }) => (
+  <svg 
+    viewBox="0 0 24 24" 
+    fill="currentColor" 
+    stroke="none"
+    className={className}
+  >
+    <path d="M12 0L14.59 9.41L24 12L14.59 14.59L12 24L9.41 14.59L0 12L9.41 9.41L12 0Z" />
+  </svg>
+);
+
+export default SparkleIcon;
