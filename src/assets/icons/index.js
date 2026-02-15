6e88bb6849319908bedd3aeb99537aafa1a31720
roopnarayankushwaha0-a11y <roopnarayankushwaha0@gmail.com>
2026-02-15 10:59:27 +0530

    Create index.js

---
+++ src/assets/icons/index.js
@@ -0,0 +1,30 @@
+/**
+ * 🎨 Icon Registry
+ * Central export file for all SVG icons.
+ * This keeps imports clean in components.
+ */
+
+// Note: We are creating these files individually next, 
+// but this file acts as a clean entry point if used.
+// The code below assumes named exports from individual files.
+
+export { default as PalmIcon } from './PalmIcon';
+export { default as TarotIcon } from './TarotIcon';
+export { default as HeartIcon } from './HeartIcon';
+export { default as StarIcon } from './StarIcon';
+export { default as SunIcon } from './SunIcon';
+export { default as MoonIcon } from './MoonIcon';
+export { default as ChatIcon } from './ChatIcon';
+export { default as SettingsIcon } from './SettingsIcon';
+export { default as HomeIcon } from './HomeIcon';
+export { default as BackIcon } from './BackIcon';
+export { default as CameraIcon } from './CameraIcon';
+export { default as UploadIcon } from './UploadIcon';
+export { default as ShareIcon } from './ShareIcon';
+export { default as SparkleIcon } from './SparkleIcon';
+export { default as CloseIcon } from './CloseIcon';
+export { default as CheckIcon } from './CheckIcon';
+export { default as ArrowRightIcon } from './ArrowRightIcon';
+export { default as GalleryIcon } from './GalleryIcon';
+export { default as RefreshIcon } from './RefreshIcon';
+// ZodiacIcons handles multiple exports usually, or we export specifically
